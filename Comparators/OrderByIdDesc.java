e456c8b1b5a4c958da3d52c4f90d2443d40fc504
mansi0204-eng <mansikumawat42137@gmail.com>
2024-09-28 13:22:34 +0530

    Update OrderByIdDesc.java

--- Comparators/OrderByIdDesc.java
+++ Comparators/OrderByIdDesc.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class OrderByIdDesc implements Comparator<Employee> {
 
-	@Override
+	//@Override
 	public int compare(Employee o1, Employee o2) {
 		return o2.getId() - o1.getId();
 	}
