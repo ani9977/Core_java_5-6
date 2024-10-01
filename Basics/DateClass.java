6322c76533d9e78ecbfd31bd7b4d634f0469e5b6
Prince837274 <pr837274@gmail.com>
2024-10-01 12:47:23 +0530

    ok

--- Basics/DateClass.java
+++ Basics/DateClass.java
@@ -10,10 +10,10 @@ public class DateClass {
 		System.out.println(d);
 		System.out.println(d.getTime());
 		System.out.println(d.getHours());
-		System.out.println(d.getMinutes());
+		//System.out.println(d.getMinutes());
 		System.out.println(d.getMonth());
 		System.out.println(d.getSeconds());
-		System.out.println(d.getTimezoneOffset());
+	//	System.out.println(d.getTimezoneOffset( hello bhaiyu));
 		System.out.println(d.getYear());
 		
 		
